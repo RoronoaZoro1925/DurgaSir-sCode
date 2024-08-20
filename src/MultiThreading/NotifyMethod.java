b7bacd23bf2e8af1c724b29296a2a67a054b1b94
RoronoaZoro1925 <sarthak1999vyas@gmail.com>
2024-08-20 20:48:30 +0530

    Interrupt & Notify

---
+++ src/MultiThreading/NotifyMethod.java
@@ -0,0 +1,36 @@
+package MultiThreading;
+
+public class WaitMethod
+{
+    public static void main(String[] args) throws InterruptedException {
+        ThreadA a = new ThreadA();
+        a.start();
+        synchronized (a)
+        {
+            System.out.println("Main thread trying to call wait() method");
+            a.wait();
+            System.out.println("Main thread got notification");
+            System.out.println(a.total);
+        }
+    }
+}
+class ThreadA extends Thread
+{
+    int total = 0;
+
+    @Override
+    public void run()
+    {
+        synchronized (this)
+        {
+            System.out.println("Child Thread to start code");
+            for (int i = 0; i <10 ; i++)
+            {
+                total=total++;
+            }
+            System.out.println("Child Thread trying to give notification");
+            this.notify();
+        }
+
+    }
+}
